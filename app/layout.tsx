39e0775580f3552feec90b5077341e5b273fa5a4
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-05-28 10:58:22 -0300

    Add AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AuthProvider>
+          {children}
+          <Toaster
+              position="top-center"
+              reverseOrder={true}
+              containerStyle={{
+                  fontSize : 16,
+              }}
+              toastOptions={{
+                  duration : 2500,
+              }}
+          />
+        </AuthProvider>
       </body>
     </html>
   );
