8d67372eeed0cdd8fa7c07ce1cc358ba94bd57e4
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-05-29 17:07:06 -0300

    Create new state for control modal; importing ModalConfirmation

--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -6,13 +6,14 @@ import toast from 'react-hot-toast';
 import styles from "./styles.module.scss";
 import { useAuth } from "../context/AuthContext";
 import { NumericFormat } from 'react-number-format';
+import ModalConfirmation from '@/components/ModalConfirmation';
 import useCurrencyFormatter from '../hooks/useCurrencyFormatter';
 
 export default function DepositPage() {
-    const [valor, setValor]       = useState('');
-    const [loading, setLoading]   = useState(false);
-    const { user, fetchUserData } = useAuth();
-    // console.log(user)
+    const [valor, setValor]                                 = useState('');
+    const [loading, setLoading]                             = useState(false);
+    const { user, fetchUserData }                           = useAuth();
+    const [openModalConfirmation, setOpenModalConfirmation] = useState<boolean>(false);
 
     const handleChange = (values: any) => {
         setValor(values.value);
@@ -45,6 +46,7 @@ export default function DepositPage() {
 
             toast.success(response.data.message);
             setValor(''); // Limpa o campo após o sucesso
+            setOpenModalConfirmation(false);
             await fetchUserData(); // Atualiza os dados do usuário, incluindo o novo saldo
         } catch (error: any) {
             console.error('Erro ao realizar depósito:', error);
@@ -58,40 +60,67 @@ export default function DepositPage() {
         }
     };
 
-    return(
-        <div className={styles.depositPage}>
-            <section className={styles.operationFormContainer}>
-                <h1>Depositar Saldo</h1>
-                <div className={styles.formCard}>
-                    <p className={styles.currentBalanceInfo}>Seu saldo atual é:
-                        <span className={styles.currentBalanceValue}>
-                            {useCurrencyFormatter(user?.balance.toFixed(2))}
-                        </span>
-                    </p>
+    const handleOpenModal = () => {
+        if(valor.length >= 4) {
+            setOpenModalConfirmation(true);    
+        } else {
+            toast.error("Preencha o campo!")
+        }
+    }
 
-                    <form className={styles.depositForm} onSubmit={handleSubmit}>
-                        <div className={styles.formGroup}>
-                            <label htmlFor="amount">Valor do Depósito:</label>
-                            <NumericFormat
-                                id="input-moeda"
-                                value={valor}
-                                onValueChange={handleChange}
-                                thousandSeparator="."
-                                decimalSeparator=","
-                                prefix="R$ "
-                                decimalScale={2}
-                                fixedDecimalScale={true}
-                                allowNegative={false}
-                                placeholder="R$ 0,00"
-                                required
-                            />
-                        </div>
-                        <button type="submit" className={styles.submitButton} disabled={loading}>
-                            {loading ? 'Processando...' : 'Confirmar Depósito'}
-                        </button>
-                    </form>
-                </div>
-            </section>
-        </div>
+    const handleCloseModal = () => {
+        setOpenModalConfirmation(false);
+    };
+
+    return(
+        <>
+            <div className={styles.depositPage}>
+                <section className={styles.operationFormContainer}>
+                    <h1>Depositar Saldo</h1>
+                    <div className={styles.formCard}>
+                        <p className={styles.currentBalanceInfo}>Seu saldo atual é:
+                            <span className={styles.currentBalanceValue}>
+                                {useCurrencyFormatter(user?.balance.toFixed(2))}
+                            </span>
+                        </p>
+
+                        <form className={styles.depositForm}>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="amount">Valor do Depósito:</label>
+                                <NumericFormat
+                                    id="input-moeda"
+                                    value={valor}
+                                    onValueChange={handleChange}
+                                    thousandSeparator="."
+                                    decimalSeparator=","
+                                    prefix="R$ "
+                                    decimalScale={2}
+                                    fixedDecimalScale={true}
+                                    allowNegative={false}
+                                    placeholder="R$ 0,00"
+                                    required
+                                />
+                            </div>
+                            <button
+                                type='button'
+                                className={styles.submitButton}
+                                onClick={handleOpenModal}
+                            >
+                                {loading ? 'Processando...' : 'Confirmar Depósito'}
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </div>
+
+            {openModalConfirmation &&
+                <ModalConfirmation
+                    data={valor}
+                    close={handleCloseModal}
+                    confirmation={handleSubmit}
+                    message={"Desejar realizar o Deposito no valor"}
+                />
+            }
+        </>
     );
 }
--- app/transfer/page.tsx
+++ app/transfer/page.tsx
@@ -7,11 +7,13 @@ import styles from "./styles.module.scss";
 import { useAuth } from "../context/AuthContext";
 import { NumericFormat } from 'react-number-format';
 import useCurrencyFormatter from "../hooks/useCurrencyFormatter";
+import ModalConfirmation from "@/components/ModalConfirmation";
 
 export default function TransferPage() {
     const [amount, setAmount]                                 = useState('');
     const { user, fetchUserData }                             = useAuth();
     const [isLoading, setIsLoading]                           = useState(false);
+    const [openModalTransfers, setOpenModalTransfers]         = useState<boolean>(false);
     const [recipientAccountNumber, setRecipientAccountNumber] = useState('');
 
     const handleAmountChange = (values: any) => {
@@ -51,6 +53,7 @@ export default function TransferPage() {
                 toast.success(response.data.message);
                 setRecipientAccountNumber(''); // Limpa o campo do destinatário
                 setAmount(''); // Limpa o campo do valor
+                setOpenModalTransfers(false);
                 fetchUserData(); // Atualiza o saldo do usuário no contexto
             } catch (error: any) {
                 console.error('Erro na transferência:', error);
@@ -62,52 +65,81 @@ export default function TransferPage() {
         }
     };
 
-    return(
-        <div className={styles.transferPage}>
-            <section className={styles.operationFormContainer}>
-                <h1>Transferir Saldo</h1>
-                <div className={styles.formCard}>
-                    <p className={styles.currentBalanceInfo}>Seu saldo atual é:
-                        <span className={styles.currentBalanceValue}>
-                            {useCurrencyFormatter(user?.balance.toFixed(2))}
-                        </span>
-                    </p>
+    const handleOpenModalTransfer = () => {
+        if(amount.length >= 4 && recipientAccountNumber.length == 4) {
+            setOpenModalTransfers(true);    
+        } else {
+            toast.error("Preencha os campos!")
+        }
+    }
 
-                    <form className={styles.depositForm} onSubmit={handleTransfer}>
-                        <div className={styles.formGroup}>
-                            <label htmlFor="amount">ID do Usuário Destino:</label>
-                            <input
-                                type="text"
-                                id="recipient-account-number"
-                                name="recipientAccountNumber"
-                                placeholder="Ex: 123456"
-                                value={recipientAccountNumber}
-                                onChange={(e) => setRecipientAccountNumber(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label htmlFor="amount">Valor do Transferência:</label>
-                            <NumericFormat
-                                id="input-moeda"
-                                value={amount}
-                                onValueChange={handleAmountChange}
-                                thousandSeparator="."
-                                decimalSeparator=","
-                                prefix="R$ "
-                                decimalScale={2}
-                                fixedDecimalScale={true}
-                                allowNegative={false}
-                                placeholder="R$ 0,00"
-                                required
-                            />
-                        </div>
-                        <button type="submit" className={styles.submitButton} disabled={isLoading}>
-                            {isLoading ? 'Transferindo...' : 'Confirmar Transferência'}
-                        </button>
-                    </form>
-                </div>
-            </section>
-        </div>
+    const handleCloseModalTransfer = () => {
+        setOpenModalTransfers(false);
+    };
+
+    return(
+        <>
+            <div className={styles.transferPage}>
+                <section className={styles.operationFormContainer}>
+                    <h1>Transferir Saldo</h1>
+                    <div className={styles.formCard}>
+                        <p className={styles.currentBalanceInfo}>Seu saldo atual é:
+                            <span className={styles.currentBalanceValue}>
+                                {useCurrencyFormatter(user?.balance.toFixed(2))}
+                            </span>
+                        </p>
+
+                        <form className={styles.depositForm}>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="amount">ID do Usuário Destino:</label>
+                                <input
+                                    type="text"
+                                    id="recipient-account-number"
+                                    name="recipientAccountNumber"
+                                    placeholder="Ex: 123456"
+                                    value={recipientAccountNumber}
+                                    maxLength={4}
+                                    onChange={(e) => setRecipientAccountNumber(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="amount">Valor do Transferência:</label>
+                                <NumericFormat
+                                    id="input-moeda"
+                                    value={amount}
+                                    onValueChange={handleAmountChange}
+                                    thousandSeparator="."
+                                    decimalSeparator=","
+                                    prefix="R$ "
+                                    decimalScale={2}
+                                    fixedDecimalScale={true}
+                                    allowNegative={false}
+                                    placeholder="R$ 0,00"
+                                    required
+                                />
+                            </div>
+                            <button
+                                type="button"
+                                disabled={isLoading}
+                                className={styles.submitButton}
+                                onClick={handleOpenModalTransfer}
+                            >
+                                {isLoading ? 'Transferindo...' : 'Confirmar Transferência'}
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </div>
+
+            {openModalTransfers &&
+                <ModalConfirmation
+                    data={amount}
+                    confirmation={handleTransfer}
+                    close={handleCloseModalTransfer}
+                    message={"Desejar realizar essa Transferência no valor"}
+                />
+            }
+        </>
     );
 }
