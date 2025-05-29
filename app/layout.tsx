8385ee49483613944f4443854d5cc4ea4cca406e
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-05-28 21:36:26 -0300

    Adding header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,16 +29,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>
+          <Header />
           {children}
           <Toaster
-              position="top-center"
-              reverseOrder={true}
-              containerStyle={{
-                  fontSize : 16,
-              }}
-              toastOptions={{
-                  duration : 2500,
-              }}
+            position="top-center"
+            reverseOrder={true}
+            containerStyle={{
+              fontSize : 16,
+            }}
+            toastOptions={{
+              duration : 2500,
+            }}
           />
         </AuthProvider>
       </body>
